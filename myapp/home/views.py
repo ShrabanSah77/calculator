fce2c110c4c7c2f61bc827483303ddead7a65490
ShrabanSah77 <shrabanshah77@gmail.com>
2026-03-09 00:18:03 -0400

    change

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def calculator (request):
+    if request.method == 'POST':
+        result = request.POST.get('result', '')
+
+        return render(request, 'index.html', {'result': result})
+    return render(request, 'index.html')
+
+
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('home page')
-
-def profile(request):
-    return HttpResponse('profile page')
+from home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('profile/', profile),
+    path('', calculator, name = 'calculator'),
 ]
